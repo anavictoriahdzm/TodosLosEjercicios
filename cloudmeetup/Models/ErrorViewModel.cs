6a83f8c130c47edc50e460e3be6981ea6cb25ea4
AnaHernandez <anav.hernandez@softtek.com>
2020-08-21 12:24:19 -0500

    Falto cloudmeetup

---
+++ cloudmeetup/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cloudmeetup.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
